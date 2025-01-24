8ee989f0e07a68adf6f87c35677ab67c6132725f
curder <q.curder@gmail.com>
2025-01-24 11:31:44 +0800

    add shopping cart HTML template with book list and price formatting

---
+++ 02-jsx/10.books.js
@@ -0,0 +1,6 @@
+let books = [
+    { id: 1, name: "Rust程序设计", date: "2023-09-01", price: 89.90, count: 1 },
+    { id: 2, name: "Python编程三剑客", date: "2020-04-01", price: 132.56, count: 2 },
+    { id: 3, name: "Java从入门到精通", date: "2023-06-01", price: 44.45, count: 1 },
+    { id: 4, name: '用Go语言开发命令行程序', date: '2024-07-01', price: 70.39, count: 1 },
+]
