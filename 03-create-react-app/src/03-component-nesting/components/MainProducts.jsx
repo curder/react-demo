6b8977b81f10870c0e46bbfa6af1a45c940d80d6
curder <q.curder@gmail.com>
2025-02-05 15:16:25 +0800

    feat: update MainProducts component to display product list

--- 03-create-react-app/src/03-component-nesting/components/MainProducts.jsx
+++ 03-create-react-app/src/03-component-nesting/components/MainProducts.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 export class MainProducts extends Component {
   render() {
     return (
-      <div className='main-products'>Product List</div>
+      <div className='main-products'>
+        <h2>商品列表</h2>
+        <ul>
+          <li>商品01</li>
+          <li>商品02</li>
+          <li>商品03</li>
+          <li>商品04</li>
+        </ul>
+      </div>
     )
   }
 }
