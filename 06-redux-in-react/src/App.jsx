15d6800d6fe1ddf55ebfdfb87b8daffc8501d411
curder <q.curder@gmail.com>
2025-02-19 17:05:17 +0800

    feat: add redux-thunk middleware and refactor App component to use async action for fetching home data

--- 06-redux-in-react/src/App.jsx
+++ 06-redux-in-react/src/App.jsx
@@ -2,21 +2,13 @@ import React, { PureComponent } from 'react'
 import './style.css'
 import Home from './components/Home'
 import Profile from './components/Profile'
-import axios from 'axios'
 import { connect } from 'react-redux'
-import { changeBannersAction, changeRecommendsAction } from './store/actionCreators'
+import { fetchHomeMultiDataAction } from './store/actionCreators'
 
 export class App extends PureComponent {
 
   componentDidMount() {
-    const url = `http://123.207.32.32:8000/home/multidata`
-    axios.get(url).then(({ data }) => {
-      return { banner: data.data.banner.list, recommend: data.data.recommend.list }
-    }).then(({ banner, recommend }) => {
-      const { changeBanners, changeRecommend } = this.props
-      changeBanners(banner)
-      changeRecommend(recommend)
-    })
+    this.props.fetchHomeMultiData()
   }
 
   render() {
@@ -39,12 +31,9 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  changeBanners(banners) {
-    dispatch(changeBannersAction(banners))
-  },
-  changeRecommend(recommends) {
-    dispatch(changeRecommendsAction(recommends))
-  },
+  fetchHomeMultiData() {
+    dispatch(fetchHomeMultiDataAction())
+  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
--- 06-redux-in-react/src/store/actionCreators.js
+++ 06-redux-in-react/src/store/actionCreators.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { CHANGE_COUNT, CHANGE_BANNERS, CHANGE_RECOMMENDS } from "./constans"
 
 export const changeCountAction = (number) => ({
@@ -14,3 +15,18 @@ export const changeRecommendsAction = (recommends) => ({
     type: CHANGE_RECOMMENDS,
     recommends,
 })
+
+// 异步 action
+export const fetchHomeMultiDataAction = () => {
+    return (dispatch, getState) => {
+        const url = `http://123.207.32.32:8000/home/multidata`
+        axios.get(url)
+            .then(
+                ({ data }) => ({ banner: data.data.banner.list, recommend: data.data.recommend.list })
+            ).then(
+                ({ banner, recommend }) => {
+                    dispatch(changeBannersAction(banner))
+                    dispatch(changeRecommendsAction(recommend))
+                })
+    }
+}
