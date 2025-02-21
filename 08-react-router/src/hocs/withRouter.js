36666a28c6019ff6a581fff2dc76cab60fdf4d6e
curder <q.curder@gmail.com>
2025-02-21 13:57:13 +0800

    feat: enhance withRouter HOC to include query parameters from URL

--- 08-react-router/src/hocs/withRouter.js
+++ 08-react-router/src/hocs/withRouter.js
@@ -1,10 +1,12 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 export default function withRouter(WrapperComponent) {
     return function (props) {
         const navigate = useNavigate();
         const params = useParams();
+        const { searchParams } = useSearchParams();
+        const query = Object.fromEntries(searchParams);
 
-        const router = { navigate, params };
+        const router = { navigate, params, query};
 
         return <WrapperComponent {...props} router={router} />;
     };
