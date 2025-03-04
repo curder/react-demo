32716a64fe6594337b706ae33f7c0e7bc3c3e367
curder <q.curder@gmail.com>
2025-03-04 09:36:19 +0800

    feat: implement routing with lazy-loaded views for Home, Entire, and Detail components

--- 10-airbnb/src/App.jsx
+++ 10-airbnb/src/App.jsx
@@ -1,8 +1,16 @@
 import React, { memo } from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from './routes'
 
 const App = memo(() => {
     return (
-        <div>App</div>
+        <div className='app'>
+            <div className="header">Header</div>
+            <div className="contant">
+                {useRoutes(routes)}
+            </div>
+            <div className="footer">div</div>
+        </div>
     )
 })
 
--- 10-airbnb/src/index.js
+++ 10-airbnb/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import "normalize.css";
 import '@/assets/index.less';
 import App from '@/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
 );
 
