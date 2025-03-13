5ef344198415dd9c100619c16ad1b202e92a45d3
curder <q.curder@gmail.com>
2025-03-13 10:55:09 +0800

    feat(react - music): add craco-less integration and implement global styles with LESS

--- 11-react-music/craco.config.js
+++ 11-react-music/craco.config.js
@@ -1,7 +1,9 @@
+const cracoLessPlugin = require('craco-less')
 const path = require('path')
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
+  plugins: [{ plugin: cracoLessPlugin }],
   webpack: {
     alias: {
       '@': resolve('src/')
