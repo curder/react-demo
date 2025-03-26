2833c5c8599951b50dc4e1b8ba1a577b93f555e3
curder <q.curder@gmail.com>
2025-03-26 18:38:49 +0800

    feat(footer): Update footer content and styles
    The commit adds new images, modifies footer links' titles and URLs in local - data.ts, and updates the footer component in index.tsx. This is done to enhance the footer functionality and visual appearance, providing new features like music open platform, AI - related services, and more intuitive navigation links. Also, new CSS styles for the footer are added in style.ts to support the new design.

--- 11-react-music/src/components/footer/index.tsx
+++ 11-react-music/src/components/footer/index.tsx
@@ -1,15 +1,64 @@
 import { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-
+import {
+  FooterCopyrightWrapper,
+  FooterEnterWrapper,
+  FooterWrapper
+} from './style'
+import { footerLinks } from '@/assets/data/local-data'
 interface FooterProps {
   children?: ReactNode
 }
 
 const Footer: FC<FooterProps> = (props) => {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <FooterWrapper className="wrap-v2">
+      <FooterEnterWrapper>
+        <li>
+          <a
+            href="#"
+            className="sprite_footer_enter logo logo-open-platform"
+          ></a>
+          <span>音乐开放平台</span>
+        </li>
+        <li>
+          <a href="#" className="sprite_footer_enter logo logo-trade"></a>
+          <span>云村交易所</span>
+        </li>
+        <li>
+          <a href="#" className="sprite_footer_enter logo logo-xstudio"></a>
+          <span>X StudioAI歌手</span>
+        </li>
+        <li>
+          <a href="#" className="sprite_footer_enter logo logo-auth"></a>
+          <span>用户认证</span>
+        </li>
+        <li>
+          <a href="#" className="sprite_footer_enter logo logo-musician"></a>
+          <span>AI 免费写歌</span>
+        </li>
+        <li>
+          <a href="#" className="sprite_footer_enter logo logo-cloudsong"></a>
+          <span>云推歌</span>
+        </li>
+        <li>
+          <a href="#" className="sprite_footer_enter logo logo-reward"></a>
+          <span>赞赏</span>
+        </li>
+      </FooterEnterWrapper>
+      <FooterCopyrightWrapper className="copyright">
+        <ul className="links">
+          {footerLinks.map((link, index) => {
+            return (
+              <li key={index}>
+                <a href={link.link}>{link.title}</a>
+                <span className="line">|</span>
+              </li>
+            )
+          })}
+        </ul>
+      </FooterCopyrightWrapper>
+    </FooterWrapper>
   )
 }
 
---
+++ 11-react-music/src/components/footer/style.ts
@@ -0,0 +1,138 @@
+import styled from 'styled-components'
+
+export const FooterWrapper = styled.div`
+  //
+`
+export const FooterEnterWrapper = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 33px;
+
+  li {
+    width: 45px;
+    margin-left: 80px;
+    text-align: center;
+    color: #666;
+
+    &:first-child {
+      margin-left: 0;
+    }
+
+    .logo {
+      display: block;
+      float: none;
+      width: 45px;
+      height: 45px;
+      margin: 0 auto;
+      background-size: 220px 220px;
+    }
+
+    // 音乐开放平台
+    .logo-open-platform {
+      background-position: -170px -5px;
+      &:hover {
+        background-position: -5px -115px;
+      }
+    }
+    // 云村交易所
+    .logo-trade {
+      background-position: -5px -170px;
+      &:hover {
+        background-position: -60px -170px;
+      }
+    }
+
+    // X StudioAI歌手
+    .logo-xstudio {
+      background-size: 45px;
+      background-image: url(${require('@/assets/images/xStudio.png')});
+      background-position: 0 0;
+
+      &:hover {
+        background-image: url(${require('@/assets/images/xStudioHover.png')});
+      }
+    }
+
+    // 用户认证
+    .logo-auth {
+      background-position: -60px -60px;
+      &:hover {
+        background-position: -115px -5px;
+      }
+    }
+
+    // AI 免费写歌
+    .logo-musician {
+      background-size: 45px;
+      background-image: url(${require('@/assets/images/aiLogo.png')});
+      background-position: 0 0;
+
+      &:hover {
+        background-image: url(${require('@/assets/images/aiLogoHover.png')});
+      }
+    }
+
+    // 云推歌
+    .logo-cloudsong {
+      background-size: 45px;
+      background-image: url(${require('@/assets/images/cloudSong.png')});
+      background-position: 0 0;
+
+      &:hover {
+        background-image: url(${require('@/assets/images/cloudSongHover.png')});
+      }
+    }
+
+    // 赞赏
+    .logo-reward {
+      background-position: -170px -115px;
+      &:hover {
+        background-position: -60px -115px;
+      }
+    }
+
+    span {
+      display: inline-block;
+      width: 100px;
+      margin-top: 10px;
+      margin-left: -27.5px;
+      font-weight: 400;
+      font-size: 12px;
+      text-align: center;
+      color: rgba(0, 0, 0, 0.5);
+    }
+  }
+`
+
+export const FooterCopyrightWrapper = styled.div`
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  margin-top: 60px;
+
+  .links {
+    display: flex;
+    line-height: 24px;
+    text-align: center;
+
+    li {
+      display: flex;
+
+      a {
+        color: #666;
+      }
+
+      .line {
+        margin: 0 8px 0 8px;
+        color: #d9d9d9;
+      }
+
+      &:last-child {
+        .line {
+          display: none;
+        }
+      }
+    }
+  }
+`
