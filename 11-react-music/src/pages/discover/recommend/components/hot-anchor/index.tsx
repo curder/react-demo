aba972c2e257c3efaf0e27668b0470c2b66f3683
curder <q.curder@gmail.com>
2025-03-26 17:44:26 +0800

    fix(local-data & hot-anchor components): Rename hotRadios to hotAnchors and enhance hot-anchor component
    The code changes rename the variable 'hotRadios' to 'hotAnchors' in local-data.ts. In the hot-anchor component, it imports the new 'hotAnchors' variable, logs it, and uses it to display a list of anchors. Also, style changes are made to format the anchor list. This was likely done to clarify variable naming and improve the display of hot anchors.

--- 11-react-music/src/pages/discover/recommend/components/hot-anchor/index.tsx
+++ 11-react-music/src/pages/discover/recommend/components/hot-anchor/index.tsx
@@ -2,15 +2,34 @@ import { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { HotAnchorWrapper } from './style'
 import RightSectionHeader from '../right-section-header'
+import { hotAnchors } from '@/assets/data/local-data'
 
 interface HotAnchorProps {
   children?: ReactNode
 }
 
 const HotAnchor: FC<HotAnchorProps> = (props) => {
-  return <HotAnchorWrapper>
-    <RightSectionHeader title="热门主播" />
-  </HotAnchorWrapper>
+  console.log(hotAnchors)
+  return (
+    <HotAnchorWrapper>
+      <RightSectionHeader title="热门主播" />
+      <div className="anchor-list">
+        {hotAnchors.map((anchor) => {
+          return (
+            <div className="anchor" key={anchor.picUrl}>
+              <a href="/discover/djradio" className="image">
+                <img src={anchor.picUrl} alt="" />
+              </a>
+              <div className="info">
+                <div className="name">{anchor.name}</div>
+                <div className="position text-no-wrap">{anchor.position}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </HotAnchorWrapper>
+  )
 }
 
 export default memo(HotAnchor)
--- 11-react-music/src/pages/discover/recommend/components/hot-anchor/style.tsx
+++ 11-react-music/src/pages/discover/recommend/components/hot-anchor/style.tsx
@@ -1,5 +1,36 @@
 import styled from 'styled-components'
 
 export const HotAnchorWrapper = styled.div`
-  //
+  margin-top: 30px;
+
+  .anchor-list {
+    margin-top: 20px;
+    padding: 20px;
+
+    .anchor {
+      display: flex;
+      margin-bottom: 10px;
+      width: 210px;
+      .image {
+        img {
+          width: 40px;
+          height: 40px;
+        }
+      }
+
+      .info {
+        width: 160px;
+        margin-left: 8px;
+        line-height: 21px;
+        .name {
+          color: #000;
+          margin-top: 3px;
+        }
+
+        .position {
+          color: #666;
+        }
+      }
+    }
+  }
 `
